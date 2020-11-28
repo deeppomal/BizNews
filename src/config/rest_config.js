932427fea842301cd78b072c3f43fd6c47d0b8c7
Deep Pomal <deeppomal98@gmail.com>
2020-11-28 14:34:14 +0530

    Update rest_config.js

--- src/config/rest_config.js
+++ src/config/rest_config.js
@@ -1,4 +1,4 @@
 export const baseUrl='https://newsapi.org/v2/top-headlines?';
 export const country = 'in';
-export const apiKey='ecf6e7ba12b340e9b63dd4cae391beab';
+export const apiKey='your-api-key';
 export const category='business'
